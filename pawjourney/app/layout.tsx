673ea257d9e019e703fa90dbb51de821db0dc676
MTRFYMD <mtrfymd@gmail.com>
2024-08-04 01:07:22 +0900

    login機能を作成（途中

---
+++ pawjourney/app/layout.tsx
@@ -0,0 +1,34 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import SupabaseListerner from "./components/supabase-listerner"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Supabase Auth',
+  description: 'Supabase Auth',
+}
+
+// レイアウト
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html>
+      <body className={inter.className}>
+        <div className="flex flex-col min-h-screen">
+          {/* @ts-expect-error next version of TS will fix this */}
+          <SupabaseListerner/>
+          
+          <main className="flex-1 container max-w-screen-sm mx-auto px-1 py-5">{children}</main>
+
+          <footer className="py-5">
+            <div className="text-center text-sm">
+              Copyright © All rights reserved | Paw Jounrney
+            </div>
+          </footer>
+        </div>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ pawjourney/app/page.tsx
@@ -0,0 +1,24 @@
+import {createServerComponentClient} from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+
+import type { Database } from "@/lib/database.types"
+
+// メインページ
+const Home = async () => {
+  const supabase = createServerComponentClient<Database>({
+    cookies,
+  })
+
+  //セッションの取得
+  const{
+    data:{session},
+  } = await supabase.auth.getSession()
+
+  return (
+    <div className="text-center text-xl">
+      {session ? <div>ログイン済み</div> : <div>未ログイン状態</div>}
+    </div>
+  )
+}
+
+export default Home
